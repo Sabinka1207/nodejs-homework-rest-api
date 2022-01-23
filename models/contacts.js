4fa6d32dcbcdff6fa7c6290c6e7ba891caa2d752
Sabina Abdulaliieva <sabinka1207@gmail.com>
2022-01-23 18:54:28 +0200

    test commit

--- models/contacts.js
+++ models/contacts.js
@@ -1,12 +1,44 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require("path")
 
-const listContacts = async () => {}
+const filePath = path.join(__dirname, "contacts.json")
 
-const getContactById = async (contactId) => {}
+async function listContacts() {
+  const data = await fs.readFile(filePath)
+  const arr = JSON.parse(data)
+  return arr
+}
 
-const removeContact = async (contactId) => {}
+async function getContactById(contactId) {
+  const allContacts = await listContacts();
+  const contact = allContacts.filter(item => item.id === `${contactId}`);
+  if (contact.length === 0 ) {
+    return null;
+  }
+  return contact;
+}
 
-const addContact = async (body) => {}
+async function removeContact(contactId) {
+  const allContacts = await listContacts();
+  const newContactList = allContacts.filter(item => item.id !== `${contactId}`);
+  if (newContactList.length === allContacts.length ) {
+    return null;
+  }
+  await fs.writeFile(filePath, JSON.stringify(newContactList));
+  return newContactList;
+}
+
+// const addContact = async (body) => {}
+
+async function addContact(name, email, phone) {
+  const allContacts = await listContacts();
+  const generatedId = allContacts.length + 1
+  console.log("generatedId = ", generatedId)
+  const data = {id: `${generatedId}`, name, email, phone};
+  allContacts.push(data);
+  await fs.writeFile(filePath, JSON.stringify(allContacts));
+  return data;
+}
 
 const updateContact = async (contactId, body) => {}
 
